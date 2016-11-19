de00f3364f3ec47ecea3a4563e6548979737be97
irieadam <irieadam5@hotmail.com>
2016-11-19 12:58:20 +0100

    process route implemented

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ var app = express();
 var PORT = process.env.PORT || 3000
 var soap = require('soap');
 var requests = [];
-    
+var results = [];
+
 // middleware
 app.use(bodyParser.json());
 
@@ -14,23 +15,49 @@ app.get('/', function (req, res) {
 });
 
 app.post('/process', function (req, res) {
+
+    var vatServiceWSDLUrl = 'http://ec.europa.eu/taxation_customs/vies/checkVatService.wsdl';    
+    var requestid = req.body.requestId;
+    var vatNumbers = req.body.vatNumbers;
+    var checkVatApprox = {};
+    var counter = 0;
+
+    // validations
+    res.status(200).send();
     
-    var requests = req.body;
+    // process list
+    vatNumbers.forEach(function (vatRequest) {
 
-    console.log(requests);
+        var checkVatApprox = {
+            countryCode : vatRequest.countryCode,
+            vatNumber : vatRequest.vatNumber,
+            requesterCountryCode : vatRequest.requesterCountryCode,
+            requesterVatNumber : vatRequest.requesterVatNumber
+            };
+        
+        soap.createClient(vatServiceWSDLUrl, function(err, client) {
+            client.checkVatApprox(checkVatApprox, function(err, result) {
+                if (result.valid) { 
+                    vatRequest.status = '2';
+                    } else if (!result.valid) {
+                        vatRequest.status = '5'
+                    } else {
+                        varRequest.status = '4'
+                    };
+                vatRequest.confirmationNumber = result.requestIdentifier;
+                results.push(vatRequest);
+                counter=counter+1;
 
-    var vatServiceWSDLUrl = 'http://ec.europa.eu/taxation_customs/vies/checkVatService.wsdl';
-    var args = {};
-
-      soap.createClient(vatServiceWSDLUrl, function(err, client) {
-      client.checkVatApprox(args, function(err, result) {
-          console.log(result);
-          console.log(result.requestIdentifier);
-      });          
-    });    
-  res.status(418).send();     
+                if (counter === vatNumbers.length) {
+                    console.log(results);
+                   // res.status(200).send();
+                }  
+            });          
+        });
+    });      
 });
 
+//start
 app.listen(PORT, function () {
     console.log('Express listening on port ' + PORT);
-})
+});
