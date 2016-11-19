8a146db383fab5ff5a7b3e007b73833120fa36cf
irieadam <irieadam5@hotmail.com>
2016-11-19 11:15:24 +0100

    added body parser

--- server.js
+++ server.js
@@ -1,44 +1,38 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 var PORT = process.env.PORT || 3000
 var soap = require('soap');
-var requests = [{
-        requestId : '12456789',
-        itemId : '111222333',
-        countryCode : 'NL',
-        vatNumber : '819381548B01',
-        requesterCountryCode : 'NL',
-        requesterVatNumber : '855356650B01'
-    },
-    {
-        requestId : '12456789',
-        itemId : '111222444',
-        countryCode : 'NL',
-        vatNumber : '819381548B99',
-        requesterCountryCode : 'NL',
-        requesterVatNumber : '855356650B01'
-
-    }];
+var requests = [];
     
+// middleware
+app.use(bodyParser.json());
 
+// routes
 app.get('/', function (req, res) {
     res.send('Velcome to ze Vatcave')
 });
 
-app.get('/process', function (req, res) {
-    res.send('processing');
+app.post('/process', function (req, res) {
+    
+    var requests = req.body;
+
+    console.log(requests);
+
     var vatServiceWSDLUrl = 'http://ec.europa.eu/taxation_customs/vies/checkVatService.wsdl';
     var args = {
             countryCode : 'NL',
             vatNumber : '819381548B01',
             requesterCountryCode : 'NL',
             requesterVatNumber : '855356650B01'};
+
       soap.createClient(vatServiceWSDLUrl, function(err, client) {
       client.checkVatApprox(args, function(err, result) {
           console.log(result);
           console.log(result.requestIdentifier);
       });          
-    });       
+    });    
+  res.status(418).send();     
 });
 
 app.listen(PORT, function () {
